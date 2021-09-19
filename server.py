80625801ecd4765ba700d93f524f45e41686fb48
csstowell <charlottestowell@berkeley.edu>
2021-09-19 07:56:36 +0000

    Updated html pages by adding styling

--- server.py
+++ server.py
@@ -358,9 +358,13 @@ def handle_logout():
 #--------------------------- NEW ------------------------------------------#
 
 API_KEY = '94016bc42734493084e87bba6984b963'
-############################## SPOONACULAR API ###################################
+
+###################   SPOONACULAR API    ###################################
+
+
 @app.route('/', methods=['GET', 'POST'])
 def recipes():
+    """Returns recipes based on ingredient"""
     if request.method == 'POST':
         content = requests.get(
             "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
@@ -375,35 +379,6 @@ def recipes():
 ############################## SPOONACULAR API ###################################
 
 
-
-"""creat a helping function parse the complex API response 
-    then forms a simpler dictionary of only the data needed 
-    from the API response"""
-
-def parse_api_results(complex_results):
-
-    recipe_details = {}
-    ingredients = []
-
-    recipe_details['title'] = complex_results['title']
-    recipe_details['image'] = complex_results['image']
-    recipe_details['servings'] = complex_results['servings']
-    recipe_details['readyInMinutes'] = complex_results['readyInMinutes']
-    recipe_details['sourceUrl'] = complex_results['sourceUrl']
-    # recipe_details['instructions'] = complex_results['instructions']..we need a different way to get the steps
-    
-    for each_step in complex_results['analyzedInstructions'][0]['steps']:
-        instructions = each_step['step']
-    recipe_details['instructions'] = instructions
-
-    
-    for each_ingredient in complex_results["extendedIngredients"]:
-        ingredient = each_ingredient["originalString"]
-        ingredients.append(ingredient)
-    # print(ingredients)   
-    recipe_details["ingredients"] = ingredients
-
-    return recipe_details
 
 
 
